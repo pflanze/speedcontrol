5021afa04246e16dc44e1357f2a1bbf7849e0c62
Christian Jaeger <chrjae@gmail.com>
2010-02-20 13:44:51 -0500

    wget http://safari.iki.fi/speedcontrol.c
    linked from http://www.thinkwiki.org/wiki/How_to_set_optical_drive_speed
    
    "The speed of the optical drive is set separately for CD-ROM and DVD,
     and is reset at reboot and resume from suspend."
    
    "*  DVD: use
     speedcontrol -x num
    
     The speedcontrol utility has source code available.
    
     For recent ThinkPad models running Linux kernels older than 2.6.15,
     this may require the libata pass-through patch.
    "
    
    M 2008-10-19 17:54 speedcontrol.c

--- speedcontrol.c
+++ speedcontrol.c
@@ -1,6 +1,6 @@
 /*
  * SpeedControl - use SET STREAMING command to set the speed of DVD-drives
- *       
+ *
  *
  * Copyright (c) 2004	Thomas Fritzsche <tf@noto.de>
  *
@@ -20,27 +20,38 @@
  *
  */
 
+/*
+ * Changelog
+ *
+ * 20081020 Sami Farin
+ *  - Add fflush(stdout) (write errors to stdout ignored, still)
+ *  - use strerror() to show open() failures
+ *  - return 1 instead of -1 in main()
+ *  - comment formatting fixes
+ *  - remove useless '& 0xff'
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <errno.h>
 #include <linux/cdrom.h>
 
- 
 void dump_sense(unsigned char *cdb, struct request_sense *sense)
 {
   int i;
-  
+
   printf("Command failed: ");
-    
+
   for (i=0; i<12; i++)
     printf("%02x ", cdb[i]);
-          
+
     if (sense) {
-      printf(" - sense: %02x.%02x.%02x\n", sense->sense_key, sense->asc,
-              sense->ascq);
+      printf(" - sense: %02x.%02x.%02x\n",
+              sense->sense_key, sense->asc, sense->ascq);
     } else {
       printf(", no sense\n");
     }
@@ -49,51 +60,50 @@ void dump_sense(unsigned char *cdb, struct request_sense *sense)
 int main(int argc, char *argv[])
 {
   char *device = "/dev/cdrom";
-  int c,fd;
+  int c, fd;
   int speed = 0;
   unsigned long rw_size;
-  
   unsigned char buffer[28];
-  
   struct cdrom_generic_command cgc;
   struct request_sense sense;
   extern char * optarg;
 
-  while((c=getopt(argc,argv,"x:"))!=EOF) {
+  while((c = getopt(argc,argv,"x:")) != EOF) {
     switch(c) {
       case 'x': speed = atoi(optarg); break;
       default:
-        printf("Usage: speedcontrol [-x speed] [device]");
-        return -1;
+        printf("Usage: %s [-x speed] [device]\n", argv[0]);
+        fflush(stdout);
+        return 1;
     }
   }
 
   if (argc > optind) device = argv[optind];
-  
-  fd = open(device, O_RDONLY | O_NONBLOCK);
-  if (fd < 0) {
-    printf("Can't open device %s\n", device);
-    return -1;
-  }
 
+  fd = open(device, O_RDWR | O_NONBLOCK);
+  if (fd < 0) {
+    printf("%s: Can't open device %s: %s\n", argv[0], device, strerror(errno));
+    fflush(stdout);
+    return 1;
+  }
 
   memset(&cgc, 0, sizeof(cgc));
   memset(&sense, 0, sizeof(sense));
   memset(&buffer, 0, sizeof(buffer));
 
- /* SET STREAMING command */ 
+  /* SET STREAMING command */
   cgc.cmd[0] = 0xb6;
- /* 28 byte parameter list length */
-  cgc.cmd[10] = 28; 
+  /* 28 byte parameter list length */
+  cgc.cmd[10] = 28;
 
   cgc.sense = &sense;
   cgc.buffer = buffer;
   cgc.buflen = sizeof(buffer);
   cgc.data_direction = CGC_DATA_WRITE;
   cgc.quiet = 1;
-  
+
   if(speed == 0) {
-/* set Restore Drive Defaults */  
+    /* set Restore Drive Defaults */
     buffer[0] = 4;
   }
 
@@ -104,32 +114,36 @@ int main(int argc, char *argv[])
 
   rw_size = 177 * speed;
 
-/* read size */  
-  buffer[12] = (rw_size >> 24) & 0xff;
-  buffer[13] = (rw_size >> 16) & 0xff;
-  buffer[14] = (rw_size >>  8) & 0xff;
-  buffer[15] = rw_size & 0xff;
+  /* read size */
+  buffer[12] = (rw_size >> 24);
+  buffer[13] = (rw_size >> 16);
+  buffer[14] = (rw_size >>  8);
+  buffer[15] = rw_size;
 
-/* read time 1 sec. */
+  /* read time 1 sec. */
   buffer[18] = 0x03;
   buffer[19] = 0xE8;
 
-/* write size */
-  buffer[20] = (rw_size >> 24) & 0xff;
-  buffer[21] = (rw_size >> 16) & 0xff;
-  buffer[22] = (rw_size >>  8) & 0xff;
-  buffer[23] = rw_size & 0xff;
+  /* write size */
+  buffer[20] = (rw_size >> 24);
+  buffer[21] = (rw_size >> 16);
+  buffer[22] = (rw_size >>  8);
+  buffer[23] = rw_size;
 
-/* write time 1 sec. */
+  /* write time 1 sec. */
   buffer[26] = 0x03;
   buffer[27] = 0xE8;
- 
-  if (ioctl(fd, CDROM_SEND_PACKET, &cgc) != 0)       
+
+  if (ioctl(fd, CDROM_SEND_PACKET, &cgc) != 0)
     if (ioctl(fd, CDROM_SELECT_SPEED, speed) != 0) {
-      dump_sense(cgc.cmd, cgc.sense);    
+      dump_sense(cgc.cmd, cgc.sense);
       printf("ERROR.\n");
-      return -1;
+      fflush(stdout);
+      return 1;
     }
+
   printf("OK...\n");
+  fflush(stdout);
   return 0;
 }
+
